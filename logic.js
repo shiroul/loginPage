78776e2946538ac33c03f0c2565cb67f0433238a
shiroul <mjamiulakbar@gmail.com>
2024-10-30 12:13:50 +0700

    first commit

---
+++ logic.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#signup").click(function() {
+        $(".signup").css({"webkitTransform":"rotateY(0deg)",
+            "MozTransform":"rotateY(0deg)",
+            "msTransform":"rotateY(0deg)",
+            "OTransform":"rotateY(0deg)",
+            "transform":"rotateY(0deg)"});
+    });
+});
